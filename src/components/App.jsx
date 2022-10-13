6737ec30b5599e32ba27dd46c96f832f80306248
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-10-13 20:17:51 +0300

    add fix commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import {Button} from './Button/Button';
-import {Modal} from './Modal/Modal';
+// import {Modal} from './Modal/Modal';
 
 export class App extends Component {
   state = {
